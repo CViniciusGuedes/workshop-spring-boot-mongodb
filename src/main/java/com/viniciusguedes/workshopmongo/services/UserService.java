97cbd72533a8472c362663774f1d5ae59d40a47c
Vinicius Guedes <c.vinicius.gr@gmail.com>
2023-12-14 23:41:48 -0300

    Obtendo usuario por id

--- src/main/java/com/viniciusguedes/workshopmongo/services/UserService.java
+++ src/main/java/com/viniciusguedes/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.viniciusguedes.workshopmongo.domain.User;
 import com.viniciusguedes.workshopmongo.repository.UserRepository;
+import com.viniciusguedes.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +17,13 @@ public class UserService {
 
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		User user = repo.findById(id).orElse(null);
+		if(user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
 	}
 }
